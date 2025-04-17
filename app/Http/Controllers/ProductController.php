5b8b68c0ba6ba4f2813c08daeb42f62e69b6996f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-17 19:45:02 +0700

    Menambah message ketika product tidak ada

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,6 +28,12 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+        ]);
+
         $product = Product::create($request->all());
         return response()->json($product, 201);
     }
@@ -37,7 +43,15 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        return response()->json(Product::findOrFail($id));
+        $product = Product::find($id);
+
+        if (! $product) {
+            return response()->json([
+                'message' => 'product tidak ditemukan.'
+            ], 404);
+        }
+
+        return response()->json($product);
     }
 
     /**
