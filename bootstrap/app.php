3c2c602400bfda6d86f9f0769f77b100c0371ae2
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-18 16:29:40 +0700

    Mendaftarkan role middleware pada bootsrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+
+    // untuk meproteksi role yang sedan login
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
