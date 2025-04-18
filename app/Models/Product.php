f006fecb75efd28b5cd6eba8175bbfbd3162127f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-18 13:42:20 +0700

    Menambah relasi antar table product dan user

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'user_id'];
+
+    // relasi antara table user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
