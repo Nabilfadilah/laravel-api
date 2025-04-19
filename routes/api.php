9dbf56a5fcde0215507c5df4110ddbe311572561
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-19 20:34:41 +0700

    Menambah route user dan middleware role admin

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum', 'role:admin')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
+    // auth
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // product management (semua user bisa CRUD produk miliknya)
     Route::apiResource('/products', ProductController::class);
+});
+
+// Route khusus admin
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    // users management
+    Route::get('/users', [UserController::class, 'index']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::post('/users/{id}/reset-password', [UserController::class, 'resetPassword']);
 });
