00bc5b09e3b8671496720bd4491cfa4e925d780a
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-25 20:31:05 +0700

    Memberikan komen penjelasan untuk userResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -7,19 +7,26 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+    // fungsi ini akan otomatis dipanggil saat data User diubah menjadi JSON
     public function toArray(Request $request): array
     {
         return [
+            // ambil id user
             'id'       => $this->id,
+
+            // ambil nama user
             'name'     => $this->name,
+
+            // ambil email user
             'email'    => $this->email,
+
+            // ambil rolenya
             'role'     => $this->role,
+
+            // tanggal pembuatan produk dalam format ISO 8601 (standar waktu internasional)
             'created_at' => $this->created_at->toIso8601String(),
+
+            // tanggal update terakhir juga pakai format ISO
             'updated_at' => $this->updated_at->toIso8601String(),
         ];
     }
